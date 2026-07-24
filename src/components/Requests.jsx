b625f2f50539a8fbb0c2bd5839c0cd4c9cbeae92
Sanket <sanketbm.007@gmail.com>
2026-07-24 15:41:33 +0530

    accept, rejecct connection req

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,12 +1,25 @@
 import axios from "axios";
 import { BASE_URL } from "../Utils/constants";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequests } from "../Utils/requestSlice";
-import { useEffect } from "react";
+import { addRequests, removeRequest } from "../Utils/requestSlice";
+import { useEffect, useState } from "react";
 
 const Requests = () => {
   const requests = useSelector((store) => store.requests);
   const dispatch = useDispatch();
+
+  const [showButtons, setShowButtons] = useState(true);
+
+  const reviewRequest = async (status, _id) => {
+    try {
+      const res = axios.post(
+        BASE_URL + "/request/review/" + status + "/" + _id,
+        {},
+        { withCredentials: true },
+      );
+      dispatch(removeRequest(_id));
+    } catch (err) {}
+  };
 
   const fetchRequests = async () => {
     try {
@@ -26,7 +39,8 @@ const Requests = () => {
 
   if (!requests) return;
 
-  if (requests.length === 0) return <h1> No Requests Found</h1>;
+  if (requests.length === 0)
+    return <h1 className="flex justify-center my-10"> No Requests Found</h1>;
 
   return (
     <div className="text-center my-10">
@@ -57,12 +71,14 @@ const Requests = () => {
             <div className="w-1/2">
               <button
                 className="btn btn-primary mx-2"
+                onClick={() => reviewRequest("rejected", request._id)}
                 style={{ backgroundColor: "blue", width: "20%" }}
               >
                 Reject
               </button>
               <button
                 className="btn btn-secondary mx-4"
+                onClick={() => reviewRequest("accepted", request._id)}
                 style={{ backgroundColor: "hotpink", width: "20%" }}
               >
                 Accept
